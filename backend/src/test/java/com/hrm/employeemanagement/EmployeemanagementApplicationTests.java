4810873a8434707cf5b1d7c496a19710ff7fcc0e
DucGiap <giapduc2299@gmail.com>
2026-08-20 11:35:03 +0700

    chore: initialize backend and frontend

---
+++ backend/src/test/java/com/hrm/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
