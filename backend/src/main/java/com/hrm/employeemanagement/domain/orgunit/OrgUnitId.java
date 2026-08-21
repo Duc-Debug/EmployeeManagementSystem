ef52ca184174386ae830cc709d009150631cab17
DucGiap <giapduc2299@gmail.com>
2026-08-21 10:29:05 +0700

    fix: equals wrong,

--- backend/src/main/java/com/hrm/employeemanagement/domain/orgunit/OrgUnitId.java
+++ backend/src/main/java/com/hrm/employeemanagement/domain/orgunit/OrgUnitId.java
@@ -2,7 +2,7 @@ package com.hrm.employeemanagement.domain.orgunit;
 
 import java.util.Objects;
 
-public class OrgUnitId {
+public final class OrgUnitId {
     private final Long value;
 
     public OrgUnitId(Long value) {
@@ -18,14 +18,19 @@ public class OrgUnitId {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
+        }
+
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
+
         OrgUnitId orgUnitId = (OrgUnitId) o;
-        return Objects.equals(o, orgUnitId.value);
+
+        return Objects.equals(value, orgUnitId.value);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(value);
