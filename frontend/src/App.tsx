60dc3c562d2e7431f259ac90b5c730daef97cf14
Hung <chu6607@gmail.com>
2026-08-24 10:07:57 +0700

    giao dien quan ly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import LoginPage from './components/auth/LoginPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginRoute from "./app/login/Page";
+import Dashboard from "./components/dashboard/Dashboard";
+import RequireAuth from "./components/auth/Requireauth";
 
 function App() {
     return (
-        <div>
-            <LoginPage />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginRoute />} />
+                <Route
+                    path="/"
+                    element={
+                        <RequireAuth>
+                            <Dashboard />
+                        </RequireAuth>
+                    }
+                />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
