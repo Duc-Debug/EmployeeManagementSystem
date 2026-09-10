c504df75153f43eaaaf140904c026ec25d053d09
Viet nguyen <nguyenvietbl98@gmail.com>
2026-09-10 10:48:33 +0700

    test: align CloneProjectWbsServiceTest with domain Role-DataScope invariants

--- backend/src/test/java/com/hrm/employeemanagement/application/service/task/CloneProjectWbsServiceTest.java
+++ backend/src/test/java/com/hrm/employeemanagement/application/service/task/CloneProjectWbsServiceTest.java
@@ -126,7 +126,7 @@ class CloneProjectWbsServiceTest {
                 new UserId(userId),
                 "pm_director",
                 "hash",
-                new Role(new RoleId(2L), RoleCode.VT_02, "PM"),
+                new Role(new RoleId(1L), RoleCode.VT_01, "Ban Giám đốc"),
                 UserStatus.ACTIVE,
                 null,
                 DataScope.COMPANY,
