e2df3a7170f48c939d7ff2580f960576e3588c5a
DucGiap <giapduc2299@gmail.com>
2026-09-15 11:44:17 +0700

    feat(application): add ports timesheet history

---
+++ backend/src/main/java/com/hrm/employeemanagement/application/port/inbound/timesheet/SendTimesheetRemindersUseCase.java
@@ -0,0 +1,5 @@
+package com.hrm.employeemanagement.application.port.inbound.timesheet;
+
+public interface SendTimesheetRemindersUseCase {
+    void sendReminders();
+}
---
+++ backend/src/main/java/com/hrm/employeemanagement/application/port/outbound/timesheet/SaveTimesheetHistoryPort.java
@@ -0,0 +1,7 @@
+package com.hrm.employeemanagement.application.port.outbound.timesheet;
+
+import com.hrm.employeemanagement.domain.timesheet.TimesheetHistory;
+
+public interface SaveTimesheetHistoryPort {
+    void save(TimesheetHistory history);
+}
